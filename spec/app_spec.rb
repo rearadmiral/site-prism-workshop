2787d79b6976cd455a0167e64e66d1616486a03c
Bill DePhillips <bill.dephillips@gmail.com>
2014-07-24 11:49:53 -0700

    added test for adding item to list using wait_for_items

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,12 +5,21 @@ Capybara.default_driver = :selenium
 
 describe 'App' do
 
-  let(:page) { AppPage.new }
+  let(:page) { AppPage.new.tap { |pg| pg.load } }
 
   it "exists" do
-    page.load
     expect(page.has_header?).to be true
   end
 
+  describe "button" do
+
+    it "adds an item when clicked" do
+      page.input.set 'Watermelon'
+      page.button.click
+      page.wait_for_items
+      expect(page.items.size).to be 1
+    end
+
+  end
 
 end
--- spec/pages/app_page.rb
+++ spec/pages/app_page.rb
@@ -3,6 +3,9 @@ require 'site_prism'
 class AppPage < SitePrism::Page
 
   element :header, 'h1'
+  elements :items, 'ul li'
+  element :input, 'input'
+  element :button, 'button'
 
   URL = "http://localhost:9292"
 
